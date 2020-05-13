f70eccef4abc0e379a8d0a565f06048e602787f2
LuzMar57 <luzmarinasoriano57@gmail.com>
2020-05-13 02:06:00 +0200

    FODCircleView: Dispatch onPress to HAL only after dimming is applied
    Change-Id: Iecdbe3bb38f003f5a7cda86b6bf3082691fd33f5

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -92,6 +92,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     private boolean mIsCircleShowing;
     private boolean mCanUnlockWithFp;
 
+    private float mCurrentDimAmount = 0.0f;
+
     private Handler mHandler;
 
     private LockPatternUtils mLockPatternUtils;
@@ -313,6 +315,16 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
                 FODCircleView.class.getSimpleName());
 
         mFODAnimation = new FODAnimation(context, mPositionX, mPositionY);
+
+        getViewTreeObserver().addOnGlobalLayoutListener(() -> {
+            float drawingDimAmount = mParams.dimAmount;
+            if (mCurrentDimAmount == 0.0f && drawingDimAmount > 0.0f) {
+                dispatchPress();
+                mCurrentDimAmount = drawingDimAmount;
+            } else if (mCurrentDimAmount > 0.0f && drawingDimAmount == 0.0f) {
+                mCurrentDimAmount = drawingDimAmount;
+            }
+        });
     }
 
     @Override
