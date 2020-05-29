376de6acf3a815a2b6929138ec08485a9a278445
RealSchnedi <schnedi3@gmail.com>
2020-05-29 12:29:02 +0200

    Revert "Welcome back MIUI brightness slider"
    This reverts commit ddc8c2669e3dfba92827bc2f60e488d27891ac74.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -695,8 +695,6 @@ public class StatusBar extends SystemUI implements DemoMode,
     private final SysuiStatusBarStateController mStatusBarStateController =
             (SysuiStatusBarStateController) Dependency.get(StatusBarStateController.class);
 
-    private boolean mUnexpandedQSBrightnessSlider;
-
     private final KeyguardUpdateMonitorCallback mUpdateCallback =
             new KeyguardUpdateMonitorCallback() {
                 @Override
@@ -2112,23 +2110,6 @@ public class StatusBar extends SystemUI implements DemoMode,
     @Override
     public void onColorsChanged(ColorExtractor extractor, int which) {
         updateTheme();
-    }
-
-    public void updateBrightnessSliderOverlay() {
-        boolean UnexpandedQSBrightnessSlider = Settings.System.getIntForUser(mContext.getContentResolver(),
-                        Settings.System.BRIGHTNESS_SLIDER_QS_UNEXPANDED, 0, UserHandle.USER_CURRENT) == 1;
-        if (mUnexpandedQSBrightnessSlider != UnexpandedQSBrightnessSlider){
-            mUnexpandedQSBrightnessSlider = UnexpandedQSBrightnessSlider;
-            mUiOffloadThread.submit(() -> {
-                final IOverlayManager mOverlayManager = IOverlayManager.Stub.asInterface(
-                                ServiceManager.getService(Context.OVERLAY_SERVICE));
-                try {
-                    mOverlayManager.setEnabled("com.extendedui.overlay.brightnessslider",
-                                mUnexpandedQSBrightnessSlider, mLockscreenUserManager.getCurrentUserId());
-                } catch (RemoteException ignored) {
-                }
-            });
-        }
     }
 
     @Nullable
@@ -4637,9 +4618,6 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.BACK_SWIPE_TYPE),
                     false, this, UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.BRIGHTNESS_SLIDER_QS_UNEXPANDED),
-                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -4681,8 +4659,6 @@ public class StatusBar extends SystemUI implements DemoMode,
                     uri.equals(Settings.System.getUriFor(Settings.System.QS_PANEL_BG_COLOR)) ||
                     uri.equals(Settings.System.getUriFor(Settings.System.QS_PANEL_BG_COLOR_WALL))) {
                 mQSPanel.getHost().reloadAllTiles();
-            } else if (uri.equals(Settings.System.getUriFor(Settings.System.BRIGHTNESS_SLIDER_QS_UNEXPANDED))) {
-                updateBrightnessSliderOverlay();
             }
             update();
         }
